f5000ad411be6b2ba674ab57a7bf78b5113f5fdf
iamminuco <alsdn5200@gmail.com>
2024-11-20 10:19:34 +0900

    Title: 정수 선언하고 뺄셈, Time: 88ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 241120/정수 선언하고 뺄셈/define-numbers-and-substract.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        int a;
+        int b;
+    
+        a = 97;
+        b = 13;
+        System.out.println(a+" - "+b+" = "+84);
+    }
+}
