dcdc78468a76a8b5947f21c95eec73f0eccd9f52
iamminuco <alsdn5200@gmail.com>
2024-11-20 15:09:09 +0900

    Title: 길이 단위 변환하기, Time: 96ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 241120/길이 단위 변환하기/change-length-unit.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        double pit = 9.2;
+        double pitToCm = 280.4;
+
+        double mi = 1.3;
+        double miToCm = 209214.2;
+
+        System.out.printf(pit+"ft = %.1fcm\n", pitToCm);
+        System.out.printf(mi+"mi = %.1fcm", miToCm);
+    }
+}
