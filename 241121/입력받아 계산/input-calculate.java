f42913313e32cca0c8877c33f907a63ddb55d932
iamminuco <alsdn5200@gmail.com>
2024-11-21 09:19:52 +0900

    Title: 입력받아 계산, Time: 114ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241121/입력받아 계산/input-calculate.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        int a;
+        Scanner sc = new Scanner(System.in);
+
+        a = sc.nextInt();
+
+        a += 2;
+        System.out.println(a);
+
+    }
+}
