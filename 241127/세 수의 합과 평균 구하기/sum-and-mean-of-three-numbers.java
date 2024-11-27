2b45da3c4d3b1dc84de56e6f9de0b221321770d1
iamminuco <alsdn5200@gmail.com>
2024-11-27 14:27:39 +0900

    Title: 세 수의 합과 평균 구하기, Time: 129ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241127/세 수의 합과 평균 구하기/sum-and-mean-of-three-numbers.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt(), b = sc.nextInt() , c = sc.nextInt(), add;
+        add = a+b+c;
+        System.out.println(add);
+        System.out.println(add/3);
+    }
+}
