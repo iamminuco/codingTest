056cb843d89894c6341ae4d08847cf81095f2e74
iamminuco <alsdn5200@gmail.com>
2024-11-29 15:47:54 +0900

    Title: 숫자의 조건 여부, Time: 149ms, Memory: 10MB, Status: Passed - CodeTree

--- 241129/숫자의 조건 여부/numbers-condition.java
+++ 241129/숫자의 조건 여부/numbers-condition.java
@@ -5,7 +5,8 @@ public class Main {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
-        if( a >= 13 ) { 
+
+        if( a >= 113 ) { 
             System.out.println(1);
         } else {
             System.out.println(0);
