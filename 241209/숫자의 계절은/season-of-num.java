dc53fdd711160c5ea73f04080274a1a89196ecf9
iamminuco <alsdn5200@gmail.com>
2024-12-09 13:44:25 +0900

    Title: 숫자의 계절은, Time: 126ms, Memory: 10MB, Status: Passed - CodeTree

--- 241209/숫자의 계절은/season-of-num.java
+++ 241209/숫자의 계절은/season-of-num.java
@@ -7,7 +7,7 @@ public class Main {
 
         int m = sc.nextInt();
 
-        if (m == 12 && m <= 2) {
+        if (m == 12 || m <= 2) {
             System.out.println("Winter");
         } else if ( m >= 9 ) {
             System.out.println("Fall");
