77a99393fdd0c34ead0d3e2eea103bb895c028ef
iamminuco <alsdn5200@gmail.com>
2024-12-09 14:40:29 +0900

    Title: 장학금, Time: 120ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241209/장학금/scholarship.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int mid = sc.nextInt();
+        int last = sc.nextInt();
+
+        if (mid >= 90) {
+            if (last >= 95) {
+              System.out.println("100000");
+            } else if (last >= 90) {
+              System.out.println("50000");
+            }
+        } else {
+            System.out.println("0");
+        }
+    }
+}
