8e459a290b0c957a8ad2935def20f83432a46259
iamminuco <alsdn5200@gmail.com>
2024-12-09 15:25:28 +0900

    Title: 두 사람, Time: 122ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241209/두 사람/two-person.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int a_age = sc.nextInt();
+        String a_sex = sc.next();
+
+        int b_age = sc.nextInt();
+        String b_sex = sc.next();
+
+        if (a_age > 19 || b_age > 19 && a_sex.equals("M") || b_sex.equals("M")) {
+            System.out.println(1);
+        } else {
+            System.out.println(0);
+        }
+    }
+}
