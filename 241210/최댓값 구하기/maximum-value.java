b587448ab8cbbe1e8f21b43a20747d353a0f9d32
iamminuco <alsdn5200@gmail.com>
2024-12-10 11:53:47 +0900

    Title: 최댓값 구하기, Time: 127ms, Memory: 10MB, Status: Passed - CodeTree

--- 241210/최댓값 구하기/maximum-value.java
+++ 241210/최댓값 구하기/maximum-value.java
@@ -8,10 +8,10 @@ public class Main {
         int b = sc.nextInt();
         int c = sc.nextInt();
         int[] arr = {a,b,c};
-        int max = 0;
+        int max = arr[0];
 
         for(int i = 0; i < arr.length; i++){
-            if(arr[i] > arr[0]){
+            if(arr[i] > max){
                 max = arr[i];
             }
         }
