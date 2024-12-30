541bf3e9d8c5c2710fdd231c405d031a976645fa
iamminuco <alsdn5200@gmail.com>
2024-12-30 10:47:12 +0900

    Title: 소수 판별기, Time: 119ms, Memory: 10MB, Status: Passed - CodeTree

--- 241230/소수 판별기/prime-number-discriminator.java
+++ 241230/소수 판별기/prime-number-discriminator.java
@@ -8,18 +8,16 @@ public class Main {
         int n = sc.nextInt();
         boolean isSide = false;
 
-        for (int i = 1; i <= n; i++) {
+        for (int i = 2; i * i <= n; i++) {
             if ( n % i == 0) {
-                if (i == 1 || i == n) {
-                    isSide = true;
-                }
+                isSide = true;
             }
         }
 
         if(isSide) {
-            System.out.println("P");
-        } else {
             System.out.println("C");
+        } else {
+            System.out.println("P");
         }
 
     }
