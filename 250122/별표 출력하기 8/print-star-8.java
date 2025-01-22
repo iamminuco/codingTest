ad5f7f81ea8e10a54848c8addaa92fbecb43a9c9
iamminuco <alsdn5200@gmail.com>
2025-01-22 11:37:24 +0900

    Title: 별표 출력하기 8, Time: 132ms, Memory: 10MB, Status: Passed - CodeTree

--- 250122/별표 출력하기 8/print-star-8.java
+++ 250122/별표 출력하기 8/print-star-8.java
@@ -3,7 +3,11 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         // Please write your code here.
-        for (int i = 0; i < 6; i++) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
             if( i % 2 == 0) {
                  System.out.print("*");
             } else {
