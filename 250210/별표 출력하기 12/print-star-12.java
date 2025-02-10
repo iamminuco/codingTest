c6ea26a9c2b2e74dd23be988e1240053aa48e918
iamminuco <alsdn5200@gmail.com>
2025-02-10 17:56:46 +0900

    Title: 별표 출력하기 12, Time: 131ms, Memory: 10MB, Status: Passed - CodeTree

--- 250210/별표 출력하기 12/print-star-12.java
+++ 250210/별표 출력하기 12/print-star-12.java
@@ -15,7 +15,7 @@ public class Main {
                         System.out.print("  ");
                     }
                 } else {
-                    if ( i >= j) {
+                    if ( i <= j) {
                         System.out.print("* ");
                     } else {
                         System.out.print("  ");
