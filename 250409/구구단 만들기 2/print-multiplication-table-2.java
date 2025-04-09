41564cc90a871fc6d4f2a79710705a4fd704d2eb
iamminuco <alsdn5200@gmail.com>
2025-04-09 16:52:57 +0900

    Title: 구구단 만들기 2, Time: 137ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250409/구구단 만들기 2/print-multiplication-table-2.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+            
+        // 4 2, 3 2, 2 2
+        // 4 4, 2 3, 3, 4
+        // 첫 숫자는 바뀌고 뒷 숫자는 고정. 
+
+
+        
+        for(int i = 2; i <= n*m; i+=2) {
+            for(int j = 4; j > 1 ; j--) {
+                System.out.print(j+" * "+i +" = "+i*j);
+            if(j > 2) {
+                System.out.print(" / ");
+            }
+            }
+            
+            System.out.println();
+        }
+        
+
+
+    }
+}
